f616cf2b21713bceda864d0c5e55b44d8f9c20aa
Pavel <GPV2002@mail.ru>
2023-03-06 02:16:09 +0300

    ref/diploma/add adaptive aboutMe

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./ui/Header/index";
 import { Main } from './Pages/Main/index';
 import { Footer } from './ui/Footer/index';
-import './index.css'
+import './index.css';
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
       <Footer/>
     </>
   );
